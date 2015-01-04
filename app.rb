7bedd423155acd140cc490eacbf4da22972a5521
Charles H Martin <charlesmartin14@gmail.com>
2015-01-03 22:38:01 -0800

    need public and tmp dirs

--- app.rb
+++ app.rb
@@ -19,13 +19,28 @@ get "/" do
   "Calculated Content Titler"
 end
 
+
+# http://xxx.xxx.xxx.xx:3456/score?="this is a tweet"
 get "/score" do
   tweet = params[:tweet]
   "the tweet to score is #{tweet}"
+
+  # we just need to load the scores
+  #
+  # toks = tweet.downcase.split
+  # vals = toks.map { |x|  REDIS["score:#{x}"] }
+  # vals.compact!
+  # vals.map! { |x| x.to_f }
+  
+  # sum = vals.inject{|sum,x| sum + x }
+  # norm = vals.to_size.to_f
+
+  # score = sum / norm
+  
 end
 
 
-# http://xxx.xxx.xxx.xx:3456/score?="this is a tweet"
+
 
 
 
